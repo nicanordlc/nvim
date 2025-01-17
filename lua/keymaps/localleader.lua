c2f68d8fc1ec7a38a83cc935aa2f51820324e838
nicanordlc <nicanordlc.dev@gmail.com>
2025-01-17 00:08:07 -0400

    add plugin coderunner + resize mappings

--- lua/keymaps/localleader.lua
+++ lua/keymaps/localleader.lua
@@ -9,6 +9,11 @@ map('n', 'q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [q]uickfix li
 map('n', ',', '<C-6>', { desc = 'Go to previous buffer' })
 map('n', 'm', '<cmd>messages<cr>', { desc = 'Show [m]essages' })
 map({ 'n', 'v' }, 'n', ':normal ', { desc = 'Enter [n]ormal mode' })
+local resize_num = 4
+map('n', 'h', '<cmd>vertical resize +' .. resize_num .. '<cr>', { desc = 'resize right' })
+map('n', 'j', '<cmd>resize +' .. resize_num .. '<cr>', { desc = 'resize down' })
+map('n', 'k', '<cmd>resize -' .. resize_num .. '<cr>', { desc = 'resize up' })
+map('n', 'l', '<cmd>vertical resize -' .. resize_num .. '<cr>', { desc = 'resize left' })
 
 -- visual
 map('v', 's', '<cmd>sort<cr>', { desc = '[s]ort' })
---
+++ lua/plugins/nica/coderunner.lua
@@ -0,0 +1,29 @@
+return {
+  'catgoose/coderunner.nvim',
+  event = 'BufReadPre',
+  keys = {
+    {
+      '<leader>cc',
+      function()
+        require('coderunner').run { split = 'horizontal' }
+      end,
+      desc = 'Run [c]ode in a split terminal',
+    },
+    {
+      '<leader>cv',
+      function()
+        require('coderunner').run { split = 'vertical' }
+      end,
+      desc = 'Run code in a [v]ertical split terminal',
+    },
+  },
+  opts = {
+    split = 'vertical',
+    scale = 0.25,
+    langs = {
+      lua = {
+        { 'luajit', '[#file]' },
+      },
+    },
+  },
+}
