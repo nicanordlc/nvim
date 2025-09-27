1658376412bf3d8d739b8681131a37375db87ec4
nicanordlc <nicanordlc.dev@gmail.com>
2025-09-27 15:28:42 -0400

    add plugin lsp-signature + vim-tmux-runner

---
+++ lua/plugins/nica/vim-tmux-runner.lua
@@ -0,0 +1,44 @@
+return {
+  'christoomey/vim-tmux-runner',
+  keys = {
+    {
+      'tr',
+      ':wa | VtrSendCommandToRunner!<cr>',
+      mode = 'n',
+      desc = '[vtr] Send command to runner',
+    },
+    {
+      'tR',
+      ':wa | VtrFlushCommand<cr>:VtrSendCommandToRunner!<cr>',
+      mode = 'n',
+      desc = '[vtr] Clear and send commant to runner',
+    },
+    {
+      'tf',
+      '<cmd>VtrFocusRunner<cr>',
+      mode = 'n',
+      desc = '[vtr] Focus runner',
+    },
+    {
+      'tk',
+      '<cmd>VtrKillRunner<cr>',
+      mode = 'n',
+      desc = '[vtr] Kill runner',
+    },
+    {
+      'ta',
+      function()
+        local pane = vim.fn.system { 'tmux', 'display', '-pF', '#P' }
+        vim.cmd(':VtrAttachToPane ' .. pane + 1, 'n', true)
+      end,
+      desc = '[vtr] Attach to next pane',
+      mode = 'n',
+    },
+    {
+      'tA',
+      ':VtrAttachToPane<space>',
+      mode = 'n',
+      desc = '[vtr], Attach to pane',
+    },
+  },
+}
