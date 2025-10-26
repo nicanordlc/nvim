80b6f6d85beaf03dc588661276f4987765295636
nicanordlc <nicanordlc.dev@gmail.com>
2025-10-26 18:55:41 -0400

    modify toggle hint command

--- lua/plugins/nica/lspconfig/keymaps.lua
+++ lua/plugins/nica/lspconfig/keymaps.lua
@@ -43,16 +43,14 @@ M.setup = function(event)
   --  For example, in C this would take you to the header.
   map('gD', vim.lsp.buf.declaration, 'Goto [D]eclaration')
 
-  -- The following code creates a keymap to toggle inlay hints in your
-  -- code, if the language server you are using supports them
-  --
-  -- This may be unwanted, since they displace some of your code
-  local client = vim.lsp.get_client_by_id(event.data.client_id)
-  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-    map('<leader>th', function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-    end, 'toggle Inlay [h]ints')
-  end
+  local hints_enabled = false
+  map('<leader>th', function()
+    hints_enabled = not hints_enabled
+    vim.diagnostic.config {
+      virtual_text = false,
+      virtual_lines = hints_enabled and { current_line = hints_enabled } or hints_enabled,
+    }
+  end, 'toggle Inlay [h]ints')
 end
 
 return M
